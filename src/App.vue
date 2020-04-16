73ed1f86943ea20a72bb8ffc907f5e9065fc5829
Mihail Georgescu <geo.mihail96@gmail.com>
2020-04-16 20:29:06 +0300

    Created initial project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <test-component />
+  </div>
+</template>
+
+<script>
+import TestComponent from "./components/TestComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    TestComponent
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <item />
+    <item />
+    <item />
+  </div>
+</template>
+
+<script>
+import Item from "./Item";
+
+export default {
+  name: "TestComponent",
+  components: { Item }
+};
+</script>
+
+<style scoped>
+</style>
